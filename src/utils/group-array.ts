0b2eeed284ded6c6418ca00e03594628267e9b0c
Sam Herrmann <samherrmann.github@gmail.com>
2020-05-08 01:24:26 +0000

    Add groupArray utility function

---
+++ src/utils/group-array.ts
@@ -0,0 +1,19 @@
+/**
+ * Groups the items of the provided array by a given key. If the `groupKey`
+ * function returns `undefined` for an item, then that item is returned in its
+ * own group. The order of the items is maintained.
+ */
+export function groupArray<T>(arr: T[], groupKey: (obj: T) => string): T[][] {
+  const groups = arr.reduce((acc, curr, index) => {
+    const key = groupKey(curr);
+    if (key) {
+      const group = acc.get(key) || [];
+      group.push(curr)
+      acc.set(key, group);
+    } else {
+      acc.set(`${index}`, [curr]);
+    }
+    return acc;
+  }, new Map<string, T[]>());
+  return Array.from(groups.values());
+}
