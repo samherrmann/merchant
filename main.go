f3788b81a73c22d4f868f325c458697b42c84a02
Sam Herrmann <samherrmann.github@gmail.com>
2021-08-29 17:49:02 -0400

    Add ability to write product IDs to CSV file

--- main.go
+++ main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"encoding/csv"
 	"encoding/json"
 	"io/ioutil"
 	"log"
+	"os"
+	"strconv"
 
 	goshopify "github.com/bold-commerce/go-shopify/v3"
 )
@@ -21,18 +24,61 @@ func main() {
 		log.Fatalf("Error parsing %v: %v", envFilename, err)
 	}
 
-	clientConfig := goshopify.App{
-		ApiKey:   config.APIKey,
-		Password: config.Password,
+	client := goshopify.NewClient(
+		goshopify.App{
+			ApiKey:   config.APIKey,
+			Password: config.Password,
+		},
+		config.ShopName,
+		"",
+	)
+
+	products := []goshopify.Product{}
+	options := &goshopify.ListOptions{
+		// 250 is the maximum limit
+		// https://shopify.dev/api/admin/rest/reference/products/product?api%5Bversion%5D=2020-10#endpoints-2020-10
+		Limit: 250,
+	}
+	for {
+		productsPacket, pagination, err := client.Product.ListWithPagination(options)
+		if err != nil {
+			log.Fatalf("Error getting list of products: %v\n", err)
+		}
+		products = append(products, productsPacket...)
+		if pagination.NextPageOptions == nil {
+			break
+		}
+		options = pagination.NextPageOptions
 	}
 
-	client := goshopify.NewClient(clientConfig, config.ShopName, "")
+	productsFilename := "products.csv"
 
-	count, err := client.Product.Count(nil)
+	productsFile, err := os.OpenFile(productsFilename, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 	if err != nil {
-		log.Fatalf("Error getting list of products: %v\n", err)
+		log.Fatalf("Error opening %v: %v", productsFilename, err)
 	}
-	log.Printf("%v\n", count)
+	defer productsFile.Close()
+
+	csvWriter := csv.NewWriter(productsFile)
+
+	header := []string{
+		"ID",
+	}
+	csvWriter.Write(header)
+
+	for _, p := range products {
+		row := []string{
+			strconv.FormatInt(p.ID, 10),
+		}
+		csvWriter.Write(row)
+	}
+
+	csvWriter.Flush()
+	if err := csvWriter.Error(); err != nil {
+		log.Fatalf("CSV write error: %v", err)
+	}
+
+	log.Println("Export was successful! :)")
 }
 
 type Config struct {
