66f463421439856a2c23427d6dfb803e56f1f121
Sam Herrmann <samherrmann.github@gmail.com>
2021-08-29 17:24:53 -0400

    Add ability to get product count

--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module github.com/samherrmann/goshopctl
 
 go 1.16
+
+replace github.com/bold-commerce/go-shopify/v3 => github.com/samherrmann/go-shopify/v3 v3.11.2
+
+require github.com/bold-commerce/go-shopify/v3 v3.11.0
--- main.go
+++ main.go
@@ -1,7 +1,42 @@
 package main
 
-import "log"
+import (
+	"encoding/json"
+	"io/ioutil"
+	"log"
+
+	goshopify "github.com/bold-commerce/go-shopify/v3"
+)
 
 func main() {
-	log.Println("Hello World!")
+	envFilename := "goshopctl.json"
+
+	bytes, err := ioutil.ReadFile(envFilename)
+	if err != nil {
+		log.Fatalf("Error reading %v: %v\n", envFilename, err)
+	}
+
+	config := &Config{}
+	if err := json.Unmarshal(bytes, config); err != nil {
+		log.Fatalf("Error parsing %v: %v", envFilename, err)
+	}
+
+	clientConfig := goshopify.App{
+		ApiKey:   config.APIKey,
+		Password: config.Password,
+	}
+
+	client := goshopify.NewClient(clientConfig, config.ShopName, "")
+
+	count, err := client.Product.Count(nil)
+	if err != nil {
+		log.Fatalf("Error getting list of products: %v\n", err)
+	}
+	log.Printf("%v\n", count)
+}
+
+type Config struct {
+	ShopName string `json:"shopName"`
+	APIKey   string `json:"apiKey"`
+	Password string `json:"password"`
 }
