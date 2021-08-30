7f30431697d1512fc342ef0e6baaf60008f063c3
Sam Herrmann <samherrmann.github@gmail.com>
2021-08-29 20:48:35 -0400

    Attach metafields to products

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/csv"
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"log"
 	"os"
@@ -31,6 +32,7 @@ func main() {
 		},
 		config.ShopName,
 		"",
+		goshopify.WithRetry(3),
 	)
 
 	products := []goshopify.Product{}
@@ -44,6 +46,25 @@ func main() {
 		if err != nil {
 			log.Fatalf("Error getting list of products: %v\n", err)
 		}
+
+		for i, product := range productsPacket {
+			log.Printf("Getting metafields for product %v\n", product.ID)
+			metafields, err := client.Product.ListMetafields(product.ID, nil)
+			if err != nil {
+				log.Fatalf("Error getting metafields for product %v: %v", product.ID, err)
+			}
+			productsPacket[i].Metafields = metafields
+
+			for j, variant := range product.Variants {
+				log.Printf("Getting metafields for variant %v\n", variant.ID)
+				metafields, err := client.Variant.ListMetafields(variant.ID, nil)
+				if err != nil {
+					log.Fatalf("Error getting metafields for variant %v: %v", variant.ID, err)
+				}
+				productsPacket[i].Variants[j].Metafields = metafields
+			}
+		}
+
 		products = append(products, productsPacket...)
 		if pagination.NextPageOptions == nil {
 			break
@@ -51,6 +72,25 @@ func main() {
 		options = pagination.NextPageOptions
 	}
 
+	if err := writeJSONFile(products); err != nil {
+		log.Fatalln(err)
+	}
+
+	log.Println("Export was successful! :)")
+}
+
+func writeJSONFile(products []goshopify.Product) error {
+	bytes, err := json.Marshal(products)
+	if err != nil {
+		return err
+	}
+	if err := ioutil.WriteFile("products.json", bytes, 0644); err != nil {
+		return err
+	}
+	return nil
+}
+
+func writeCSVFile(products []goshopify.Product) {
 	productsFilename := "products.csv"
 
 	productsFile, err := os.OpenFile(productsFilename, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
@@ -62,13 +102,34 @@ func main() {
 	csvWriter := csv.NewWriter(productsFile)
 
 	header := []string{
-		"ID",
+		"id",
+		// "handle",
+		// "title",
+		// "vendor",
+		// "body_html",
+		// "product_type",
+		// "tags",
+		// Options                        []ProductOption `json:"options,omitempty"`,
+		// Variants                       []Variant       `json:"variants,omitempty"`,
+		// Image                          Image           `json:"image,omitempty"`,
+		// Images                         []Image         `json:"images,omitempty"`,
+		// TemplateSuffix                 string          `json:"template_suffix,omitempty"`,
+		// MetafieldsGlobalTitleTag       string          `json:"metafields_global_title_tag,omitempty"`,
+		// MetafieldsGlobalDescriptionTag string          `json:"metafields_global_description_tag,omitempty"`,
+		"metafields",
 	}
 	csvWriter.Write(header)
 
 	for _, p := range products {
 		row := []string{
-			strconv.FormatInt(p.ID, 10),
+			fmt.Sprintf("%q", strconv.FormatInt(p.ID, 10)),
+			// fmt.Sprintf("%q", p.Handle),
+			// fmt.Sprintf("%q", p.Title),
+			// fmt.Sprintf("%q", p.Vendor),
+			// fmt.Sprintf("%q", p.BodyHTML),
+			// fmt.Sprintf("%q", p.ProductType),
+			// fmt.Sprintf("%q", p.Tags),
+			fmt.Sprintf("%q", p.Metafields),
 		}
 		csvWriter.Write(row)
 	}
@@ -77,8 +138,6 @@ func main() {
 	if err := csvWriter.Error(); err != nil {
 		log.Fatalf("CSV write error: %v", err)
 	}
-
-	log.Println("Export was successful! :)")
 }
 
 type Config struct {
