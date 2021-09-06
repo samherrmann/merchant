aa7560d6b2d6e7f103cf073ea8a8b3c5e2f671f1
Sam Herrmann <samherrmann.github@gmail.com>
2021-09-05 21:48:48 -0400

    Add ability to open cache file in VSCode

---
+++ cmd/cache_open.go
@@ -0,0 +1,30 @@
+package cmd
+
+import (
+	"os/exec"
+	"path/filepath"
+
+	"github.com/spf13/cobra"
+)
+
+func init() {
+	cacheCmd.AddCommand(openCmd)
+}
+
+var openCmd = &cobra.Command{
+	Use:   "open",
+	Short: "Open file from cache in Visual Studio Code",
+	Args:  cobra.ExactArgs(1),
+	RunE: func(cmd *cobra.Command, args []string) error {
+		dir, err := cacheDir()
+		if err != nil {
+			return err
+		}
+		return runVSCode(filepath.Join(dir, args[0]))
+	},
+}
+
+func runVSCode(filename string) error {
+	cmd := exec.Command("code", filename)
+	return cmd.Run()
+}
