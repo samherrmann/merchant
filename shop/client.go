589a0f133f65890f5b221e45d184cf83adf44054
Sam Herrmann <samherrmann.github@gmail.com>
2023-03-18 14:40:44 -0400

    Add more methods to shop.Client

--- shop/client.go
+++ shop/client.go
@@ -2,10 +2,18 @@
 package shop
 
 import (
+	"errors"
 	"fmt"
+	"os"
 
 	goshopify "github.com/bold-commerce/go-shopify/v3"
+	"github.com/samherrmann/shopctl/cache"
 	"github.com/samherrmann/shopctl/config"
+	"github.com/samherrmann/shopctl/memdb"
+)
+
+var (
+	ErrNotExist = errors.New("does not exist")
 )
 
 type Client struct {
@@ -26,6 +34,15 @@ func NewClient(c *config.StoreConfig) *Client {
 	}
 }
 
+func (c *Client) GetProduct(id int64, skipCache bool) (*goshopify.Product, error) {
+	p, err := c.getProduct(id, skipCache)
+	// Fall back to pulling product from store if no cache exists.
+	if !skipCache && os.IsNotExist(err) {
+		return c.getProduct(id, true)
+	}
+	return p, err
+}
+
 func (c *Client) GetProductWithMetafields(id int64) (*goshopify.Product, error) {
 	product, err := c.Product.Get(id, nil)
 	if err != nil {
@@ -37,8 +54,29 @@ func (c *Client) GetProductWithMetafields(id int64) (*goshopify.Product, error) 
 	return product, nil
 }
 
+func (c *Client) GetVariantBySKU(sku string) (*goshopify.Variant, error) {
+	return c.searchVariant(func(v *goshopify.Variant) bool {
+		return v.Sku == sku
+	})
+}
+
+func (c *Client) GetVariantByBarcode(barcode string) (*goshopify.Variant, error) {
+	return c.searchVariant(func(v *goshopify.Variant) bool {
+		return v.Barcode == barcode
+	})
+}
+
+func (c *Client) GetInventory(skipCache bool) ([]goshopify.Product, error) {
+	p, err := c.getInventory(skipCache)
+	// Fall back to pulling product from store if no cache exists.
+	if !skipCache && os.IsNotExist(err) {
+		return c.getInventory(true)
+	}
+	return p, err
+}
+
 func (c *Client) GetInventoryWithMetafields() ([]goshopify.Product, error) {
-	products, err := c.ListProducts(nil)
+	products, err := c.listProducts(nil)
 	if err != nil {
 		return nil, err
 	}
@@ -58,7 +96,7 @@ func (c *Client) GetVariantCount() (int, error) {
 	options := &goshopify.ListOptions{
 		Fields: "variants",
 	}
-	products, err := c.ListProducts(options)
+	products, err := c.listProducts(options)
 	if err != nil {
 		return 0, err
 	}
@@ -71,7 +109,75 @@ func (c *Client) GetVariantCount() (int, error) {
 	return len(variantIds), nil
 }
 
-func (c *Client) ListProducts(options *goshopify.ListOptions) ([]goshopify.Product, error) {
+// UpdateProducts updates the given products in the store.
+func (c *Client) UpdateProducts(products []goshopify.Product) error {
+	// Get latest inventory from live store so that we don't accidentally make
+	// updates based on an outdated cache.
+	inventory, err := c.GetInventory(true)
+	if err != nil {
+		return err
+	}
+	db, err := memdb.New(inventory)
+	if err != nil {
+		return err
+	}
+	operations, err := db.Operations(products)
+	if err != nil {
+		return err
+	}
+	errs := []error{}
+	for _, p := range operations.NewProducts {
+		if _, err := c.Product.Create(p); err != nil {
+			errs = append(errs, err)
+		}
+	}
+	for _, p := range operations.ProductUpdates {
+		if _, err := c.Product.Update(p); err != nil {
+			errs = append(errs, err)
+		}
+	}
+	for _, v := range operations.NewVariants {
+		if _, err := c.Variant.Create(v.ProductID, v); err != nil {
+			errs = append(errs, err)
+		}
+	}
+	for _, v := range operations.VariantUpdates {
+		if _, err := c.Variant.Update(v); err != nil {
+			errs = append(errs, err)
+		}
+	}
+	return errors.Join(errs...)
+}
+
+func (c *Client) getProduct(id int64, skipCache bool) (*goshopify.Product, error) {
+	if skipCache {
+		p, err := c.Product.Get(id, nil)
+		if err != nil {
+			return nil, err
+		}
+		if err := cache.WriteProductFile(p); err != nil {
+			return nil, err
+		}
+	}
+	return cache.ReadProductFile(id)
+}
+
+// getInventory gets all products from the store and stores them in the cache
+// file.
+func (c *Client) getInventory(skipCache bool) ([]goshopify.Product, error) {
+	if skipCache {
+		p, err := c.listProducts(nil)
+		if err != nil {
+			return nil, err
+		}
+		if err := cache.WriteInventoryFile(p); err != nil {
+			return nil, err
+		}
+	}
+	return cache.ReadInventoryFile()
+}
+
+func (c *Client) listProducts(options *goshopify.ListOptions) ([]goshopify.Product, error) {
 	products := []goshopify.Product{}
 	defaultOptions := &goshopify.ListOptions{
 		// 250 is the maximum limit
@@ -114,4 +220,19 @@ func (c *Client) attachMetafields(product *goshopify.Product) error {
 		product.Variants[j].Metafields = metafields
 	}
 	return nil
+}
+
+func (c *Client) searchVariant(fn func(v *goshopify.Variant) bool) (*goshopify.Variant, error) {
+	inventory, err := c.getInventory(false)
+	if err != nil {
+		return nil, err
+	}
+	for _, p := range inventory {
+		for _, v := range p.Variants {
+			if fn(&v) {
+				return &v, nil
+			}
+		}
+	}
+	return nil, ErrNotExist
 }
