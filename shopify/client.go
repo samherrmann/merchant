11748c9e3a544d27654072e9bccdf8b24efa0f04
Sam Herrmann <samherrmann.github@gmail.com>
2023-03-26 20:33:38 -0400

    Make operations in shopify package more testable

--- shopify/client.go
+++ shopify/client.go
@@ -3,12 +3,8 @@ package shopify
 
 import (
 	"errors"
-	"fmt"
-	"os"
 
 	goshopify "github.com/bold-commerce/go-shopify/v3"
-	"github.com/samherrmann/merchant/cache"
-	"github.com/samherrmann/merchant/memdb"
 )
 
 var (
@@ -34,204 +30,45 @@ func NewClient(c *Configuration) *Client {
 }
 
 func (c *Client) GetProduct(id int64, skipCache bool) (*goshopify.Product, error) {
-	p, err := c.getProduct(id, skipCache)
-	// Fall back to pulling product from store if no cache exists.
-	if !skipCache && os.IsNotExist(err) {
-		return c.getProduct(id, true)
-	}
-	return p, err
+	return getProduct(c.Product, id, true)
 }
 
 func (c *Client) GetProductWithMetafields(id int64) (*goshopify.Product, error) {
-	product, err := c.Product.Get(id, nil)
-	if err != nil {
-		return nil, err
-	}
-	if err := c.attachMetafields(product); err != nil {
-		return nil, err
-	}
-	return product, nil
+	return getProductWithMetafields(c.Product, c.Variant, id)
 }
 
 func (c *Client) GetVariantBySKU(sku string) (*goshopify.Variant, error) {
-	return c.searchVariant(func(v *goshopify.Variant) bool {
-		return v.Sku == sku
-	})
+	return searchVariant(
+		c.Product,
+		func(v *goshopify.Variant) bool {
+			return v.Sku == sku
+		},
+	)
 }
 
 func (c *Client) GetVariantByBarcode(barcode string) (*goshopify.Variant, error) {
-	return c.searchVariant(func(v *goshopify.Variant) bool {
-		return v.Barcode == barcode
-	})
+	return searchVariant(
+		c.Product,
+		func(v *goshopify.Variant) bool {
+			return v.Barcode == barcode
+		},
+	)
 }
 
 func (c *Client) GetInventory(skipCache bool) ([]goshopify.Product, error) {
-	p, err := c.getInventory(skipCache)
-	// Fall back to pulling product from store if no cache exists.
-	if !skipCache && os.IsNotExist(err) {
-		return c.getInventory(true)
-	}
-	return p, err
+	return getInventory(c.Product, skipCache)
 }
 
 func (c *Client) GetInventoryWithMetafields() ([]goshopify.Product, error) {
-	products, err := c.listProducts(nil)
-	if err != nil {
-		return nil, err
-	}
-	for i, product := range products {
-		fmt.Printf("Getting metafields for product %v\n", product.ID)
-		if err := c.attachMetafields(&product); err != nil {
-			return nil, err
-		}
-		// TODO check if this is necessary.
-		products[i] = product
-	}
-	return products, nil
+	return getInventoryWithMetafields(c.Product, c.Variant)
 }
 
 // GetVariantCount returns the total number of variants for all products.
 func (c *Client) GetVariantCount() (int, error) {
-	options := &goshopify.ListOptions{
-		Fields: "variants",
-	}
-	products, err := c.listProducts(options)
-	if err != nil {
-		return 0, err
-	}
-	variantIds := []int64{}
-	for _, p := range products {
-		for _, v := range p.Variants {
-			variantIds = append(variantIds, v.ID)
-		}
-	}
-	return len(variantIds), nil
+	return getVariantCount(c.Product)
 }
 
 // UpdateProducts updates the given products in the store.
 func (c *Client) UpdateProducts(products []goshopify.Product) error {
-	// Get latest inventory from live store so that we don't accidentally make
-	// updates based on an outdated cache.
-	inventory, err := c.GetInventory(true)
-	if err != nil {
-		return err
-	}
-	db, err := memdb.New(inventory)
-	if err != nil {
-		return err
-	}
-	operations, err := db.Operations(products)
-	if err != nil {
-		return err
-	}
-	errs := []error{}
-	for _, p := range operations.NewProducts {
-		if _, err := c.Product.Create(p); err != nil {
-			errs = append(errs, err)
-		}
-	}
-	for _, p := range operations.ProductUpdates {
-		if _, err := c.Product.Update(p); err != nil {
-			errs = append(errs, err)
-		}
-	}
-	for _, v := range operations.NewVariants {
-		if _, err := c.Variant.Create(v.ProductID, v); err != nil {
-			errs = append(errs, err)
-		}
-	}
-	for _, v := range operations.VariantUpdates {
-		if _, err := c.Variant.Update(v); err != nil {
-			errs = append(errs, err)
-		}
-	}
-	return errors.Join(errs...)
-}
-
-func (c *Client) getProduct(id int64, skipCache bool) (*goshopify.Product, error) {
-	if skipCache {
-		p, err := c.Product.Get(id, nil)
-		if err != nil {
-			return nil, err
-		}
-		if err := cache.WriteProductFile(p); err != nil {
-			return nil, err
-		}
-	}
-	return cache.ReadProductFile(id)
-}
-
-// getInventory gets all products from the store and stores them in the cache
-// file.
-func (c *Client) getInventory(skipCache bool) ([]goshopify.Product, error) {
-	if skipCache {
-		p, err := c.listProducts(nil)
-		if err != nil {
-			return nil, err
-		}
-		if err := cache.WriteInventoryFile(p); err != nil {
-			return nil, err
-		}
-	}
-	return cache.ReadInventoryFile()
-}
-
-func (c *Client) listProducts(options *goshopify.ListOptions) ([]goshopify.Product, error) {
-	products := []goshopify.Product{}
-	defaultOptions := &goshopify.ListOptions{
-		// 250 is the maximum limit
-		// https://shopify.dev/api/admin/rest/reference/products/product?api%5Bversion%5D=2020-10#endpoints-2020-10
-		Limit: 250,
-	}
-	if options == nil {
-		options = defaultOptions
-	}
-	if options.Limit == 0 {
-		options.Limit = defaultOptions.Limit
-	}
-	for {
-		productsPacket, pagination, err := c.Product.ListWithPagination(options)
-		if err != nil {
-			return nil, fmt.Errorf("failed to get packet of products: %w", err)
-		}
-		products = append(products, productsPacket...)
-		if pagination.NextPageOptions == nil {
-			break
-		}
-		options = pagination.NextPageOptions
-	}
-	return products, nil
-}
-
-// attachMetafields fetches and attaches all metafields for the given product and its variants.
-func (c *Client) attachMetafields(product *goshopify.Product) error {
-	metafields, err := c.Product.ListMetafields(product.ID, nil)
-	if err != nil {
-		return fmt.Errorf("failed to get metafields for product %v: %w", product.ID, err)
-	}
-	product.Metafields = metafields
-
-	for j, variant := range product.Variants {
-		metafields, err := c.Variant.ListMetafields(variant.ID, nil)
-		if err != nil {
-			return fmt.Errorf("failed to get metafields for variant %v: %w", variant.ID, err)
-		}
-		product.Variants[j].Metafields = metafields
-	}
-	return nil
-}
-
-func (c *Client) searchVariant(fn func(v *goshopify.Variant) bool) (*goshopify.Variant, error) {
-	inventory, err := c.getInventory(false)
-	if err != nil {
-		return nil, err
-	}
-	for _, p := range inventory {
-		for _, v := range p.Variants {
-			if fn(&v) {
-				return &v, nil
-			}
-		}
-	}
-	return nil, ErrNotExist
+	return updateProducts(c.Product, c.Variant, products)
 }
---
+++ shopify/operations.go
@@ -0,0 +1,204 @@
+package shopify
+
+import (
+	"errors"
+	"fmt"
+	"os"
+
+	goshopify "github.com/bold-commerce/go-shopify/v3"
+	"github.com/samherrmann/merchant/cache"
+	"github.com/samherrmann/merchant/memdb"
+)
+
+type ProductService = goshopify.ProductService
+type VariantService = goshopify.VariantService
+type Product = goshopify.Product
+type Variant = goshopify.Variant
+type ListOptions = goshopify.ListOptions
+
+func getProduct(service ProductService, id int64, skipCache bool) (*Product, error) {
+	if skipCache {
+		p, err := service.Get(id, nil)
+		if err != nil {
+			return nil, err
+		}
+		if err := cache.WriteProductFile(p); err != nil {
+			return nil, err
+		}
+	}
+	product, err := cache.ReadProductFile(id)
+	// Fall back to pulling product from store if no cache exists, but only if
+	// skipCache is false to prevent an infinite loop.
+	if os.IsNotExist(err) && !skipCache {
+		return getProduct(service, id, true)
+	}
+	return product, err
+}
+
+func getProductWithMetafields(pService ProductService, vService VariantService, id int64) (*Product, error) {
+	product, err := pService.Get(id, nil)
+	if err != nil {
+		return nil, err
+	}
+	if err := attachMetafields(pService, vService, product); err != nil {
+		return nil, err
+	}
+	return product, nil
+}
+
+// getInventory gets all products from the store and stores them in the cache
+// file.
+func getInventory(service ProductService, skipCache bool) ([]Product, error) {
+	if skipCache {
+		p, err := listProducts(service, nil)
+		if err != nil {
+			return nil, err
+		}
+		if err := cache.WriteInventoryFile(p); err != nil {
+			return nil, err
+		}
+	}
+	products, err := cache.ReadInventoryFile()
+	// Fall back to pulling product from store if no cache exists, but only if
+	// skipCache is false to prevent an infinite loop.
+	if os.IsNotExist(err) && !skipCache {
+		return getInventory(service, true)
+	}
+	return products, err
+}
+
+func getInventoryWithMetafields(pService ProductService, vService VariantService) ([]Product, error) {
+	products, err := listProducts(pService, nil)
+	if err != nil {
+		return nil, err
+	}
+	for i, product := range products {
+		fmt.Printf("Getting metafields for product %v\n", product.ID)
+		if err := attachMetafields(pService, vService, &product); err != nil {
+			return nil, err
+		}
+		// TODO check if this is necessary.
+		products[i] = product
+	}
+	return products, nil
+}
+
+func listProducts(service ProductService, options *ListOptions) ([]Product, error) {
+	products := []Product{}
+	defaultOptions := &ListOptions{
+		// 250 is the maximum limit
+		// https://shopify.dev/api/admin/rest/reference/products/product?api%5Bversion%5D=2020-10#endpoints-2020-10
+		Limit: 250,
+	}
+	if options == nil {
+		options = defaultOptions
+	}
+	if options.Limit == 0 {
+		options.Limit = defaultOptions.Limit
+	}
+	for {
+		productsPacket, pagination, err := service.ListWithPagination(options)
+		if err != nil {
+			return nil, fmt.Errorf("failed to get packet of products: %w", err)
+		}
+		products = append(products, productsPacket...)
+		if pagination.NextPageOptions == nil {
+			break
+		}
+		options = pagination.NextPageOptions
+	}
+	return products, nil
+}
+
+func updateProducts(
+	pService ProductService,
+	vService VariantService,
+	products []Product,
+) error {
+	// Get latest inventory from live store so that we don't accidentally make
+	// updates based on an outdated cache.
+	inventory, err := getInventory(pService, true)
+	if err != nil {
+		return err
+	}
+	db, err := memdb.New(inventory)
+	if err != nil {
+		return err
+	}
+	operations, err := db.Operations(products)
+	if err != nil {
+		return err
+	}
+	errs := []error{}
+	for _, p := range operations.NewProducts {
+		if _, err := pService.Create(p); err != nil {
+			errs = append(errs, err)
+		}
+	}
+	for _, p := range operations.ProductUpdates {
+		if _, err := pService.Update(p); err != nil {
+			errs = append(errs, err)
+		}
+	}
+	for _, v := range operations.NewVariants {
+		if _, err := vService.Create(v.ProductID, v); err != nil {
+			errs = append(errs, err)
+		}
+	}
+	for _, v := range operations.VariantUpdates {
+		if _, err := vService.Update(v); err != nil {
+			errs = append(errs, err)
+		}
+	}
+	return errors.Join(errs...)
+}
+
+// attachMetafields fetches and attaches all metafields for the given product and its variants.
+func attachMetafields(pService ProductService, vService VariantService, product *Product) error {
+	metafields, err := pService.ListMetafields(product.ID, nil)
+	if err != nil {
+		return fmt.Errorf("failed to get metafields for product %v: %w", product.ID, err)
+	}
+	product.Metafields = metafields
+
+	for j, variant := range product.Variants {
+		metafields, err := vService.ListMetafields(variant.ID, nil)
+		if err != nil {
+			return fmt.Errorf("failed to get metafields for variant %v: %w", variant.ID, err)
+		}
+		product.Variants[j].Metafields = metafields
+	}
+	return nil
+}
+
+func searchVariant(service ProductService, fn func(v *Variant) bool) (*Variant, error) {
+	inventory, err := getInventory(service, false)
+	if err != nil {
+		return nil, err
+	}
+	for _, p := range inventory {
+		for _, v := range p.Variants {
+			if fn(&v) {
+				return &v, nil
+			}
+		}
+	}
+	return nil, ErrNotExist
+}
+
+func getVariantCount(service ProductService) (int, error) {
+	options := &ListOptions{
+		Fields: "variants",
+	}
+	products, err := listProducts(service, options)
+	if err != nil {
+		return 0, err
+	}
+	variantIds := []int64{}
+	for _, p := range products {
+		for _, v := range p.Variants {
+			variantIds = append(variantIds, v.ID)
+		}
+	}
+	return len(variantIds), nil
+}
